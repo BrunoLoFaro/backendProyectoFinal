c85a05c7cf3f1f92dca2b48d7458957345831f67
Bruno <lofarobruno@gmail.com>
2022-01-16 22:18:18 -0300

    dotenv config

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ import express from 'express';
 import { carritoRouter } from './routes/carrito.routes.js';
 import { productosRouter } from './routes/productos.routes.js';
 import { handleError } from './middleware/errorHandler.js';
-import dotenv from 'dotenv';
-
-dotenv.config({path: './config/.env'})
 
 export const app = express();
 
