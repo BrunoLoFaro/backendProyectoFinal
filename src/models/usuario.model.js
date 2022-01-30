b29b8c1aeb2d47a1a49fb43f1712e2fb6d647cee
Bruno <lofarobruno@gmail.com>
2022-01-30 19:18:50 -0300

    fixed model

--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -18,7 +18,7 @@ const usuariosSchema = new mongoose.Schema({
     apellido:{type:String, require: true, max:100},
     edad:{type:Number, require: true, max:100},
     alias:{type:String, require: true, max:100},
-    avatar:{data:Buffer, contentType:String, require: true, max:100},
+    avatar:{data:Buffer, contentType:String}
 })
 
 const Mongo = mongoose.model(usuariosCollection, usuariosSchema);
