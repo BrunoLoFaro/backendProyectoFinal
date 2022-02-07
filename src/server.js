ec2d8095a01b4719d83d521afd9167b6fba8b99c
Bruno <lofarobruno@gmail.com>
2022-02-06 23:38:42 -0300

    cluster mode. Env variable added

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import {app} from "./app.js"
 import dotenv from 'dotenv';
+import cluster from 'cluster';
+import { cpus } from 'os';
 dotenv.config({path: './src/config/.env'})
 import {logger} from './middleware/logger.config.js'
-
 const PORT = process.env.PORT
-console.log(PORT)
-const server = app.listen(PORT, ()=>{
-    
+const numCPUs = cpus().length;
+
+if (cluster.isPrimary && process.env.MULTITHREADING=='true') {
+    logger.info(`Primary ${process.pid} is running`);
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} 
+
+else {
+    const server = app.listen(PORT, ()=>{
     logger.info(`Servidor HTTP escuchando en el puerto ${PORT}`);
-});
-server.on('error', error=>logger.warn('Error en servidor', error));
+    });
+    server.on('error', error=>logger.warn('Error en servidor', error));
+}
